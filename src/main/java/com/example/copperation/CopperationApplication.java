9f55d8bf31caed123865f052c2e599a5012d176f
kevin <1932456395@qq.com>
2020-05-02 23:53:24 +0800

    Initial commit

---
+++ src/main/java/com/example/copperation/CopperationApplication.java
@@ -0,0 +1,13 @@
+package com.example.copperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopperationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/copperation/CopperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
